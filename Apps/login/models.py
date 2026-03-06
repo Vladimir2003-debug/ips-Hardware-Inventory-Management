cb4df0471a70b8100c3d26152cca96af658479ed
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2026-03-06 18:20:30 -0500

    deleted files

--- Apps/login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=100)
-    email = models.EmailField()
-    tipo = models.CharField(max_length=100)
-    contraseña = models.CharField(max_length=100)
